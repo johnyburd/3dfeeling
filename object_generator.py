580c81fd7b3f298ad4ec874472fa062a5df37147
James Johnson <jjohnsonjj1251@gmail.com>
2020-01-13 14:42:31 -0500

    Added file for object generation server and a little refactoring

---
+++ object_generator.py
@@ -0,0 +1,13 @@
+import threading
+from ShapeRepresentation import data_manipulation
+from nlp import SentimentClassifier
+
+classifier = SentimentClassifier.VADClassifier()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
