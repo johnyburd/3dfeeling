0f9d222ffc1c0ac08b5665093ffb71b8f1f814b6
Michael Burke <michaelb@lawn-128-61-114-116.lawn.gatech.edu>
2020-01-12 22:00:27 -0500

    Added blank results page

---
+++ website/src/Results.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './Results.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Results() {
+  return (
+    <div className="Results">
+      <header className="Results-header">
+        <div className="Results-display">
+        Results will be displayed here
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Results;
