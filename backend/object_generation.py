10b718466a6698bb8bff964a1b738f7b4eaf0f9b
James Johnson <jjohnsonjj1251@gmail.com>
2020-01-16 15:40:11 -0500

    Got basic asyncio threading working

--- backend/object_generation.py
+++ backend/object_generation.py
@@ -6,14 +6,15 @@ import nlp.SentimentClassifier as VAD
 vad_classifier = VAD.VADClassifier()
 
 def generate(text):
-    print(vad_classifier.analyzeSentiment(text))
+    return vad_classifier.analyzeSentiment(text)
 
 
 async def get_object(text):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future = executor.submit(generate, text)
-        await location = future.result()
-        print(location)
+    loop = asyncio.get_running_loop()
+
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        result = await loop.run_in_executor(pool, generate, text)
+        print(result)
 
 if __name__ == "__main__":
     asyncio.run(get_object("It is a cruel world out there."))
