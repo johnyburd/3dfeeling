72da37b468f08ff21d3b447358d550033592430f
Michael Burke <31456608+mburke1434@users.noreply.github.com>
2020-01-17 16:09:03 -0500

    Michael (#13)
    * Added blank results page
    
    * Changed submit and index files to account for new blank page
    
    * Added STL viewing component
    
    * Adding build to gitignore
    
    * Adding build to gitignore
    
    * Undoing unwanted build changes on my branch
    
    * Adding node_modules to gitignore because apparently I have to do that too...
    
    * Undoing node_module changes
    
    * Deleting build and node_modules folders
    
    * First page now saves text into state on submission
    
    * Took way to long but added text and prepared state variable to receive results on last page

--- website/src/App.js
+++ website/src/App.js
@@ -11,34 +11,67 @@ import {FormControl, Form, Button} from 'react-bootstrap';
 
 import PersonList from './PersonList.js';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="background">
-        <div className="item">
-        <header className="App-header">
-            <Form>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Please enter a text sample!</Form.Label>
-                <Form.Control as="textarea" size="lg" rows="6" />
-                <Link to="/submit">
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-                </Link>
-            </Form.Group>
-            </Form>
-        </header>
-        </div>
-        <div className="divider" />
-        <div className="item">
-          <p>Or choose a file to upload</p>
-          <input type="file"/>
-        </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.sendTextToAPI = this.sendTextToAPI.bind(this)
+  }
 
+  componentDidMount() {
+    console.log('Component Mounted')
+  }
+
+  handleChange(newText) {
+    this.setState({
+      text: newText,
+    })
+  }
+
+  sendTextToAPI() {
+    console.log('You should see input text below')
+    const text = this.state.text
+    console.log(text)
+
+    // fetch('api/**SEND_ME_SOMEWHERE**', {
+    //   method: 'POST',
+    //   body: text,
+    // })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <div className="background">
+          <div className="item">
+            <header className="App-header">
+                <Form onChange={() => this.handleChange(this.textInput.value)}>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Please enter a text sample!</Form.Label>
+                    <Form.Control as="textarea" size="lg" rows="6" ref={text => {this.textInput = text}}/>
+                    <Link to="/submit">
+                    <Button variant="primary" type="submit" onClick={this.sendTextToAPI}>
+                        Submit
+                    </Button>
+                    </Link>
+                </Form.Group>
+                </Form>
+            </header>
+          </div>
+          <div className="divider" />
+          <div className="item">
+            <p>Or choose a file to upload</p>
+            <input type="file"/>
+          </div>
+
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default App;
