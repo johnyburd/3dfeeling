3123c442f85ed85cc0c06c4f80761e3d84ccdfab
Jonathan Buchanan <johnyburd8@gmail.com>
2020-01-17 21:34:18 -0500

    example route

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,5 @@
+from random import uniform
+
 from aiohttp import web
 from aiohttp.web import Request, Response
 
@@ -11,6 +13,14 @@ async def analyze(request: Request) -> Response:
     print(data)
     return web.json_response({'data': 'james is dum'})
 
+
+@routes.get('/example')
+async def example(request: Request) -> Response:
+    gen = (round(uniform(0, 100), 1) for _ in range(3))
+    return web.json_response(
+        {'valence': next(gen), 'arousal': next(gen), 'dominance': next(gen)})
+
+
 app = web.Application()
 app.add_routes(routes)
-web.run_app(app)
+web.run_app(app, host='0.0.0.0', port=3999)
