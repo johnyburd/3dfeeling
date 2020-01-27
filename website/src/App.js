91aa437350d6dc1f47f5f98af51958e7ef061ecf
Michael Burke <mburke1434@gmail.com>
2020-01-27 10:11:44 -0500

    Website functional with conditional renders all on App.js. I should organize this later

--- website/src/App.js
+++ website/src/App.js
@@ -8,13 +8,21 @@ import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { ProgressBar } from 'react-bootstrap';
 import {FormControl, Form, Button} from 'react-bootstrap';
+import STLViewer from 'stl-viewer';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      text: '',
+      inputText: '',
+      loading: false,
+      loaded: false,
+      valence: '',
+      arousal: '',
+      dominance: '',
+      fileName: '',
     }
     this.handleChange = this.handleChange.bind(this)
     this.sendTextToAPI = this.sendTextToAPI.bind(this)
@@ -26,50 +34,118 @@ class App extends React.Component {
 
   handleChange(newText) {
     this.setState({
-      text: newText,
+      inputText: newText,
     })
   }
 
-  sendTextToAPI() {
+  sendTextToAPI(event) {
+    let currentComponent = this
+    let retrievedData = {
+      fileName: "Didnt work", 
+      points: [-1, -1, -1],
+    }
+    event.preventDefault()
     console.log('You should see input text below')
-    const text = this.state.text
+    const text = this.state.inputText
     console.log(text)
+    this.setState({
+      loading: true
+    })
     axios.post('https://api.3dfeeling.ga/analyze',
-      `text=${this.state.text}`
+      `text=${this.state.inputText}`
     ).then(function (resp) {
-      console.log(resp);
+      //Assign gathered sentiment analysis values here
+      console.log(resp)
+      retrievedData = resp.data
+      currentComponent.setState({
+        loading: false,
+        loaded: true,
+        valence: retrievedData.points[0][0],
+        arousal: retrievedData.points[0][1],
+        dominance: retrievedData.points[0][2],
+        fileName: retrievedData.filename
+      })
+      console.log(retrievedData);
     }).catch(function (error) {
       console.log(error);
     });
   }
 
   render () {
-    return (
-      <div className="App">
-        <div className="background">
-          <div className="item">
-            <header className="App-header">
-              <Form onChange={() => this.handleChange(this.textInput.value)}>
-              <Form.Group controlId="exampleForm.ControlTextarea1">
-                <Form.Label>Please enter a text sample!</Form.Label>
-                <Form.Control as="textarea" size="lg" rows="6" ref={text => {this.textInput = text}}/>
-                <Link to="/submit">
-                <Button variant="primary" type="submit" onClick={this.sendTextToAPI}>
-                  Submit
-                </Button>
-                </Link>
-              </Form.Group>
-              </Form>
-            </header>
-          </div>
-          <div className="divider" />
-          <div className="item">
-            <p>Or choose a file to upload</p>
-            <input type="file"/>
+    if (!this.state.loading && !this.state.loaded) {
+      return (
+        <div className="App">
+          <div className="background">
+            <div className="item">
+              <header className="App-header">
+                <Form onChange={() => this.handleChange(this.textInput.value)}>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Please enter a text sample!</Form.Label>
+                  <Form.Control as="textarea" size="lg" rows="6" ref={text => {this.textInput = text}}/>
+                  <Button variant="primary" type="submit" onClick={this.sendTextToAPI}>
+                    Submit
+                  </Button>
+                </Form.Group>
+                </Form>
+              </header>
+            </div>
+            <div className="divider" />
+            <div className="item">
+              <p>Or choose a file to upload</p>
+              <input type="file"/>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      )
+    } else if (this.state.loading && !this.state.loaded) {
+      return (
+        <div className="Submit">
+          <header className="Submit-header">
+            Thank you for submitting!
+          <div style={{ width: 400 }}>
+            <ProgressBar animated now={45} />
+            <p>Sit tight. Your text is being processed!</p>
+            <p>(Please do not refresh the page)</p>
+          </div>
+          </header>
+        </div>
+      );
+    } else if (!this.state.loading && this.state.loaded) {
+      return (
+        <div className="Results">
+          <div className="Background">
+            <div className='item'>
+              <div className='Results-header'>
+                Results and shape will be displayed here
+                <STLViewer
+                  model={'https://api.3dfeeling.ga/assets/' + this.state.fileName}
+                  //model={'https://api.3dfeeling.ga/assets/b82be92e-5fa3-4040-bf24-0afb4ec0da39.stl'}
+                  width={400}
+                  height={400}
+                  modelColor='#EAEAEA'
+                  backgroundColor='#222222'
+                  rotate={true}
+                  orbitControls={true}
+                />
+              <ul>
+                <li>
+                  Valence: { this.state.valence }%
+                </li>
+                <li>
+                  Arousal: { this.state.arousal }%
+                </li>
+                <li>
+                  Dominance: { this.state.dominance }%
+                </li>
+              </ul>
+              <p>Input text: {this.state.inputText}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+    
   }
 
 }
--- website/src/Results.js
+++ website/src/Results.js
@@ -1,56 +1,60 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Results.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import STLViewer from 'stl-viewer';
 
 // Test model? 'https://bohdanbirdie.github.io/stl-obj-demo/bottle.stl'
-import testSTL from './testSTL.stl'
 
-import ExampleGet from './ExampleGet.js';
+function Results() {
+  const [valence, setValence] = useState()
+  const [arousal, setArousal] = useState()
+  const [dominance, setDominance] = useState()
+  const [inputText, setInputText] = useState()
+  useEffect(() => {
+    axios.get(`https://api.3dfeeling.ga/example`)
+      .then(res => {
+        //const data = res.data;
+        // setData(res.data);
+        setValence(res.data.valence)
+        setArousal(res.data.arousal)
+        setDominance(res.data.dominance)
+      })
+  }, []);
 
-class Results extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      resultText: '',
-
-    }
-    this.updateText = this.updateText.bind(this)
-  }
-
-  updateText(newText) {
-    this.setState({
-      resultText: newText
-    })
-  }
-
-  render () {
-    return (
-      <div className="Results">
-        <div className="Background">
-          <div className='item'>
-            <div className='Results-header'>
-              Results and shape will be displayed here
-              <STLViewer
-                model={'https://api.3dfeeling.ga/assets/b82be92e-5fa3-4040-bf24-0afb4ec0da39.stl'}
-      //#model={'https://bohdanbirdie.github.io/stl-obj-demo/bottle.stl'}
-                width={400}
-                height={400}
-                modelColor='#EAEAEA'
-                backgroundColor='#222222'
-                rotate={true}
-                orbitControls={true}
-              />
-            <p>{this.state.resultText}</p>
-            <ExampleGet/>
-            </div>
+  return (
+    <div className="Results">
+      <div className="Background">
+        <div className='item'>
+          <div className='Results-header'>
+            Results and shape will be displayed here
+            <STLViewer
+              model={'https://api.3dfeeling.ga/assets/b82be92e-5fa3-4040-bf24-0afb4ec0da39.stl'}
+              width={400}
+              height={400}
+              modelColor='#EAEAEA'
+              backgroundColor='#222222'
+              rotate={true}
+              orbitControls={true}
+            />
+          <p>Input text: {inputText}</p>
+          <ul>
+            <li>
+              Valence: { valence }%
+            </li>
+            <li>
+              Arousal: { arousal }%
+            </li>
+            <li>
+              Dominance: { dominance }%
+            </li>
+          </ul>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Results;
