56f16165b3f163a1e2f6531b0573e70198c3d51b
Michael Burke <mburke1434@gmail.com>
2020-01-27 15:26:20 -0500

    Made modifications to results page: took out percentage symbols and put some more text. Also, values shown are averages now

--- website/src/App.js
+++ website/src/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this)
     this.sendTextToAPI = this.sendTextToAPI.bind(this)
+    this.pointsAverage = this.pointsAverage.bind(this)
   }
 
   componentDidMount() {
@@ -38,11 +39,26 @@ class App extends React.Component {
     })
   }
 
+  pointsAverage(points) {
+    let averages = [0, 0, 0]
+    let num = points.length
+    for (let i = 0; i < num; i++) {
+      averages[0] += points[i][0]
+      averages[1] += points[i][1]
+      averages[2] += points[i][2]
+    }
+    averages[0] /= num
+    averages[1] /= num
+    averages[2] /= num
+    console.log("Averages of points: " + averages)
+    return averages
+  }
+
   sendTextToAPI(event) {
     let currentComponent = this
     let retrievedData = {
       fileName: "Didnt work", 
-      points: [-1, -1, -1],
+      points: [[-1, -1, -1]],
     }
     event.preventDefault()
     console.log('You should see input text below')
@@ -57,12 +73,13 @@ class App extends React.Component {
       //Assign gathered sentiment analysis values here
       console.log(resp)
       retrievedData = resp.data
+      let averages = currentComponent.pointsAverage(retrievedData.points)
       currentComponent.setState({
         loading: false,
         loaded: true,
-        valence: retrievedData.points[0][0],
-        arousal: retrievedData.points[0][1],
-        dominance: retrievedData.points[0][2],
+        valence: averages[0],
+        arousal: averages[1],
+        dominance: averages[2],
         fileName: retrievedData.filename
       })
       console.log(retrievedData);
@@ -116,7 +133,8 @@ class App extends React.Component {
           <div className="Background">
             <div className='item'>
               <div className='Results-header'>
-                Results and shape will be displayed here
+                <h2>This is your shape is all of its glory!</h2>
+                <p>Analysis values and the text it was generated from are below.</p>
                 <STLViewer
                   model={'https://api.3dfeeling.ga/assets/' + this.state.fileName}
                   //model={'https://api.3dfeeling.ga/assets/b82be92e-5fa3-4040-bf24-0afb4ec0da39.stl'}
@@ -129,13 +147,13 @@ class App extends React.Component {
                 />
               <ul>
                 <li>
-                  Valence: { this.state.valence }%
+                  Valence: { this.state.valence }
                 </li>
                 <li>
-                  Arousal: { this.state.arousal }%
+                  Arousal: { this.state.arousal }
                 </li>
                 <li>
-                  Dominance: { this.state.dominance }%
+                  Dominance: { this.state.dominance }
                 </li>
               </ul>
               <p>Input text: {this.state.inputText}</p>
