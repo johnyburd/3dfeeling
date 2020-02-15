bfc1071c204707c2c493461bfe6dccac9f60912c
johnyburd <johnyburd8@gmail.com>
2020-02-14 20:39:18 -0500

    Release 0.2.0 (#45)
    * height function
    
    * Terrain object generation complete. Need to edit some hyperparameters still.
    
    * Tuned terrain generation
    
    * Fixed linting errors
    
    * linting
    
    * Vad graphs (#43)
    
    * Started VAD line plots
    
    * Single sentence works. Filename change. VAD graphs
    
    * james forgot this cause he's dumb
    
    * Website getting better (#44)
    
    * Website functional with conditional renders all on App.js. I should organize this later
    
    * Made modifications to results page: took out percentage symbols and put some more text. Also, values shown are averages now
    
    * Typo fix. 'Shape is all... -> shape in all'
    
    * Cleaned up duplicate code and index so that there is no unused code. (Except for 'logo' we'll use that later
    
    * Made website light themed and changed colors of STL viewer
    
    * Files can now be uploaded to API but I have only tested .txt files. User can now only choose to upload a file or submit text, not both. Kinda broken so I will organize it later"
    
    * change to scss and update stl viewer
    
    * why were these here
    
    * yarn.lock is important
    
    * Not broken because I have pleased the react gods and now text box gets disabled when there is a file uploaded
    
    * start organizing bootstrap a little
    
    * Basic lightmode color scheme implementation
    
    * scuffed scss
    
    * Improvements to viewing quality of first page. All code is wrapped within main form. Clear button implemented.
    
    * Didn't save colors.scss
    
    * Fixed typo
    
    * Fixed another typo. I'm on a roll
    
    * Results now hosted on new page. Realized through Results.js file. Uses history so that user can navigate using browser buttons. Results page data persists.
    
    * Extremely important fix to the progress bar's niceness
    
    * theme switcher
    
    * Made submit and clear buttons look nice. Added cancel button to loading screen.
    
    * better buttons
    
    * change file upload and fix buttons
    
    * dark mode button and file upload style
    
    * icon and stl background
    
    * responsive and errors no longer pause forever
    
    * fix results page
    
    Co-authored-by: johnyburd <johnyburd8@gmail.com>
    Co-authored-by: Joseph Meredith <43077689+josephmeredith@users.noreply.github.com>
    
    Co-authored-by: aschott6 <47439837+aschott6@users.noreply.github.com>
    Co-authored-by: James <jjohnsonjj1251@gmail.com>
    Co-authored-by: Michael Burke <31456608+mburke1434@users.noreply.github.com>
    Co-authored-by: Joseph Meredith <43077689+josephmeredith@users.noreply.github.com>

---
+++ website/src/Results.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { withRouter } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { STLViewer } from 'react-stl-obj-viewer';
+import DarkModeToggle from './Dark';
+
+
+class Results extends React.Component {
+
+  componentDidMount() {
+    console.log('Results Mounted')
+  }
+
+  render () {
+      return (
+        <div className="Results">
+          <div className="background">
+            <DarkModeToggle/>
+
+            <div>
+              <div className='Results-header'>
+                <h2>This is your shape in all of its glory!</h2>
+                <p>Analysis values and the text it was generated from are below.</p>
+                <STLViewer
+                  url={'https://api.3dfeeling.ga/assets/' + this.props.location.state.apiFileName}
+                  backgroundColor="#f6f8f9"
+                  sceneClassName="test-scene"
+                  modelColor='red'
+                />
+                <p>Average Values:</p>
+                <ul>
+                  <li>
+                    Valence: { this.props.location.state.valence }
+                  </li>
+                  <li>
+                    Arousal: { this.props.location.state.arousal }
+                  </li>
+                  <li>
+                    Dominance: { this.props.location.state.dominance }
+                  </li>
+                </ul>
+                <p>Input text: { this.props.location.state.inputText }</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+}
+
+export default withRouter(Results);
