e2a4398d6f0478c54f8173bc09b7ab49258ad815
James Johnson <jjohnsonjj1251@gmail.com>
2020-03-02 17:38:51 -0500

    Limit total number of VAD points to ~2000

--- backend/object_generation.py
+++ backend/object_generation.py
@@ -7,6 +7,7 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 from math import ceil
+import cProfile
 
 # libraries made by us for this project
 import nlp.SentimentClassifier as VAD
@@ -37,24 +38,21 @@ def generate(text):
     This function will fail and error out if an empty string is passed to it. Make sure there
     is at least one sentence in the string before calling this function.
     """
-    points = np.array([vad_classifier.analyzeSentiment(sentence)
-                       for sentence in sent_tokenize(text)])
-    # Need to make sure that there are at least 2 sentences and less than 25,000
-    if points.shape[0] == 1:
-        points.append(points[0])
-    elif points.shape[0] > 25000:
-        avg_points = []
-
-        window = ceil(points.shape[0] / 25000)
-        if (points.shape[0] % window) != 0:
-            points = np.append(points, [points[-1] * (points.shape[0] % window)], axis=0)
-
-        for i in range(0, points.shape[0] - window, window):
-            avg_points.append(np.sum(points[i:i + window], axis=0) / window)
-
-        points = np.array(avg_points)
-
-    print("Shape: ", points.shape)
+    points = []
+    sents = sent_tokenize(text)
+    if len(sents) == 1:
+        sents.append(sents[0])
+        points = np.array([vad_classifier.analyzeSentiment(s) for s in sents])
+    elif len(sents) > 2000:
+        window = ceil(len(sents) / 2000)
+        for i in range(0, len(sents) - window, window):
+            vals = np.array([vad_classifier.analyzeSentiment(sents[i + j]) for j in range(window)])
+            avg = np.sum(vals, axis=0) / window
+            points.append(avg)
+        for i in range(len(sents) - window, len(sents)):
+            points.append(vad_classifier.analyzeSentiment(sents[i]))
+    else:
+        points = [vad_classifier.analyzeSentiment(s) for s in sents]
 
     model = generate_terrain(points, 250)
 
@@ -85,5 +83,5 @@ async def get_object(text):
     return result
 
 if __name__ == "__main__":
-    paragraph = "This is a fun and happy sentence. " * 30000
-    asyncio.run(get_object(paragraph))
+    paragraph = "This is a fun and happy sentence. " * 32000
+    cProfile.run("generate(paragraph)")
