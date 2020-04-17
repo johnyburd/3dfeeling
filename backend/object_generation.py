78a36e05684025d49de977be5825d84bd6ac4a77
Michael Burke <mburke1434@gmail.com>
2020-04-17 12:19:56 -0400

    This note is sad. Maybe some newlines will help

--- backend/object_generation.py
+++ backend/object_generation.py
@@ -112,10 +112,38 @@ async def get_object(text):
 
 if __name__ == "__main__":
     paragraph = """
-It’s true friends.  It’s like that with so many things is life.  We spend so much time fearing things but then when we are face-to-face with them we find we had nothing to fear all along.  I’m glad to be talking to all of you now but I guess in the end I don’t need anyone to comfort or help me.  I still suppose it might have been nice to say goodbye in person instead of like this.  I suppose my advice for the living might just be: Live!  And when it is time to die, die!
-I’m still feeling fine, maybe I’m not bleeding enough.  I just don’t see how I could live indefinitely with a wound like that.  I know the people I love are going to be so upset about this but there is no need!  Please, don’t!  I’m fine and everything you should be feeling badly about is behind me now.  All of my suffering is over and it was horrible, nightmarish torture that I went through for the last year.  It’s okay now.  I have no fear, it is only sadness because of everything I still wanted to do but as I’ve said so many times all of that was gone anyway.
-Part of what I wanted in desiring to die in the company of those I loved was to reassure them and perhaps give them courage to face death well.  That was something I really wanted to give to them and I’m sorry I can only do it with these words.  I was driven almost mad by all of the things many other people said about paraplegia, suicide, and what was still possible in my condition.  I hope everyone understands how all of that affected the tone of what I wrote.  I was so frustrated with all of it, I thought it was so insane.  But I only wanted to break free of it all and say what I felt.  I felt like it stifled me so horribly.
-I cut some more and the blood is flowing well again.  I’m surprised how long it is taking me to even feel anything.  I thought I was dizzy but I’m not sure I am now.  It’s 8:51 pm.  I thought I would get cold but I’m not cold either, I’m actually hot but that’s probably the two sweaters.  Starting to feel a little badly.  Sweating, a little light-headed.
+It’s true friends.  It’s like that with so many things is life.
+We spend so much time fearing things but then when we are face-to-face
+with them we find we had nothing to fear all along.
+I’m glad to be talking to all of you now but I guess in the end
+I don’t need anyone to comfort or help me.  I still suppose it might
+have been nice to say goodbye in person instead of like this.  I
+suppose my advice for the living might just be: Live!  And when it is
+time to die, die!
+I’m still feeling fine, maybe I’m not bleeding enough.  I just don’t
+see how I could live indefinitely with a wound like that.  I know the people
+ I love are going to be so upset about this but there is no need!
+ Please, don’t!  I’m fine and everything you should be feeling badly
+ about is behind me now.  All of my suffering is over and it was horrible,
+ nightmarish torture that I went through for the last year.  It’s okay now.
+ I have no fear, it is only sadness because of everything I still
+ wanted to do but as I’ve said so many times all of that was gone anyway.
+Part of what I wanted in desiring to die in the company of those I
+loved was to reassure them and perhaps give them courage to face death
+well.  That was something I really wanted to give to them and I’m
+sorry I can only do it with these words.  I was driven almost mad by
+all of the things many other people said about paraplegia, suicide,
+and what was still possible in my condition.  I hope everyone
+understands how all of that affected the tone of what I wrote.
+I was so frustrated with all of it, I thought it was so insane.
+But I only wanted to break free of it all and say what I felt.
+I felt like it stifled me so horribly.
+I cut some more and the blood is flowing well again.
+I’m surprised how long it is taking me to even feel anything.
+I thought I was dizzy but I’m not sure I am now.  It’s 8:51 pm.
+I thought I would get cold but I’m not cold either, I’m actually
+hot but that’s probably the two sweaters.  Starting to feel a
+little badly.  Sweating, a little light-headed.
 I’m going to go now, done writing.  Goodbye everyone.
 """
     print(generate(paragraph))
